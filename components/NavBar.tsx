5a59f5c7c65da77646ec4873bdc4c2cc71f48fcf
Piotr Romanowski <piotr@romanowski.me>
2024-02-01 21:29:39 +0100

    NavBar improvements

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -26,19 +26,23 @@ const NavBar = () => {
 
   return (
     <nav
-      className={`sticky p-5 top-0 z-10 transition bg-background backdrop-blur ${
-        scrolled ? "bg-opacity-80" : "bg-opacity-100"
+      className={`fixed top-0 w-full z-10 transition bg-background max-w-screen-2xl transform -translate-x-1/2 left-1/2 ${
+        scrolled
+          ? "bg-opacity-80 backdrop-blur"
+          : "bg-opacity-0 backdrop-blur-0"
       }`}
     >
-      <div className="flex items-center">
-        <Image
-          src="vulcanova.svg"
-          alt=""
-          height="42"
-          width="42"
-          className="rounded-lg shadow-logoShadow shadow-primary/10"
-        />
-        <p className="font-bold ml-4">Vulcanova</p>
+      <div className="p-5 md:px-10">
+        <div className="flex items-center">
+          <Image
+            src="vulcanova.svg"
+            alt=""
+            height="42"
+            width="42"
+            className="rounded-lg shadow-logoShadow shadow-primary/10"
+          />
+          <p className="font-bold ml-4">Vulcanova</p>
+        </div>
       </div>
     </nav>
   );
