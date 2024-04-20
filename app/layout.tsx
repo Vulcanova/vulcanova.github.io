4fddade66a9aa44e5ea6bda94351ccbc2d56a8e6
Wiktor Stawicki <105926196+rotkiW285@users.noreply.github.com>
2024-04-20 18:20:14 +0200

    Change favicon to Vulcanova logo (#4)
    * Change favicon to Vulcanova logo
    
    * Add favicon and icons to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
       <body
         className={`${inter.className} bg-background flex max-w-screen-2xl mx-auto flex-col min-h-screen`}
       >
